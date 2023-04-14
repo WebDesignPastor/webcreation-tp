e836580217e4cc22caaa7df4ba0807bdd33172b8
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-04-14 15:28:49 +0200

    Container style added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,18 @@
 import logoTitle from "../../assets/images/logo-s.png";
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div className="container home-page">
       <div className="text-zone">
-        <h1>Hi, <br />I'm</h1>
+        <h1>Hi, <br />I'm
         <img src={logoTitle} alt="developer"/>
+        lobodan
+        <br />
+        web developer
+        </h1>
+        <h2>Full Stack Junior Developer / Ruby on Rails / HTML / CSS / Javascript </h2>
+        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
       </div>
     </div>
   )
