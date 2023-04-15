f98b88c3870824190ffcedf7874ec333823ed78b
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-04-15 17:45:25 +0200

    Imported UseState

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import logoTitle from "../../assets/images/logo-s.png";
 import './index.scss';
@@ -5,6 +6,7 @@ import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+
   const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
   const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
 
