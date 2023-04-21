67bd019668456ffeb8e01eed5a900ce80b893760
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-04-21 13:55:02 +0200

    Changed logo without draw svg

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
 import LogoS from '../../../assets/images/logo-s.png'
 import { useEffect, useRef } from 'react'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import { gsap } from 'gsap-trial'
 
 const Logo = () => {
@@ -11,18 +11,30 @@ const Logo = () => {
   const solidLogoRef = useRef();
 
   useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
+  //   gsap.registerPlugin(DrawSVGPlugin)
 
     gsap.timeline()
-    .to(bgRef.current, {
-      duration: 1,
-      opacity: 1
-    })
+  //   .to(bgRef.current, {
+  //     duration: 1,
+  //     opacity: 1
+  //   })
 
     .from(outlineLogoRef.current, {
       drawSVG: 0,
       duration: 20,
     })
+
+    gsap.fromTo(
+      solidLogoRef.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: 4,
+        duration: 3,
+      }
+    )
   },[])
 
   return (
