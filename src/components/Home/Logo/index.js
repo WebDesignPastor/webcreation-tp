4dabb9a38b590b97dd4bd4adfd27cd4cfc375b2c
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-04-21 09:52:33 +0200

    Added animation to Logo and outline

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,12 +1,29 @@
 import './index.scss'
 import LogoS from '../../../assets/images/logo-s.png'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
+import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import { gsap } from 'gsap-trial'
 
 const Logo = () => {
 
   const bgRef = useRef();
   const outlineLogoRef = useRef();
   const solidLogoRef = useRef();
+
+  useEffect(() => {
+    gsap.registerPlugin(DrawSVGPlugin)
+
+    gsap.timeline()
+    .to(bgRef.current, {
+      duration: 1,
+      opacity: 1
+    })
+
+    .from(outlineLogoRef.current, {
+      drawSVG: 0,
+      duration: 20,
+    })
+  },[])
 
   return (
     <div className="logo-container" ref={bgRef}>
