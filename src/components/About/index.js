128dbea0cf4e0ba69632c311653ff4cab91154da
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-05-04 12:00:35 +0200

    Added effect and style to about text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,23 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
 
 const About = () => {
+
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+    setLetterClass('text-animate-hover')
+  }, 4000)
+  }, [])
 
   return (
     <div className='container about-page'>
       <div className='text-zone'>
         <h1>
           <AnimatedLetters
+            letterClass={letterClass}
             strArray={['A',' ','p','r','o','p','o','s',' ','d','e',' ','m','o','i']}
             idx={15}
           />
