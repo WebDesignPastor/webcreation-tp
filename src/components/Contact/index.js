39c048a924e313ad16a77f2457a7e5f3ffbca826
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-05-09 11:16:12 +0200

    Changed text from home

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,6 +3,7 @@ import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import { useEffect, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -70,6 +71,20 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className="info-map">
+          François PARMENTIER
+          <br />
+          35132 Vezin-le-Coquet
+          <span>webcreationtp@gmail.com</span>
+        </div>
+        <div className='map-wrap'>
+          <MapContainer center={[48.11680092161994, -1.7539931866289937]} zoom={13}>
+            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+            <Marker position={[48.11680092161994, -1.7539931866289937]}>
+              <Popup>C'est ici que tout se fait :-D</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type="pacman" />
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,14 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
-import logoTitle from "../../assets/images/logo-s.png";
 import AnimatedLetters from '../AnimatedLetters';
-import Logo from './Logo'
 import './index.scss';
 import Loader from 'react-loaders';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
-  const nameArray = ['r', 'a', 'n', 'c', 'o', 'i', 's']
-  const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+  const jobArray = 'Développeur Web Full Stack'.split('')
+  const greetingArray = 'Bonjour,   je suis François'.split('')
 
   useEffect(() => {
       setTimeout(() => {
@@ -23,24 +21,17 @@ const Home = () => {
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-        <span className={letterClass}>H</span>
-        <span className={`${letterClass} _12`}>i</span>
-        <br />
-        <span className={`${letterClass} _13`}>I'</span>
-        <span className={`${letterClass} _14`}>M</span>
-        <img src={logoTitle} alt="developer"/>
         <AnimatedLetters letterClass={letterClass}
-        strArray={nameArray}
-        idx={15}/>
+        strArray={greetingArray}
+        idx={5}/>
         <br />
         <AnimatedLetters letterClass={letterClass}
         strArray={jobArray}
-        idx={22}/>
+        idx={5}/>
         </h1>
-        <h2>Full Stack Junior Developer / Ruby on Rails / HTML / CSS / Javascript </h2>
-        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+        <h2>Développeur Junior Full Stack / Ruby on Rails / HTML / CSS / Javascript / ReactJS </h2>
+        <Link to="/contact" className='flat-button'>CONTACTEZ-MOI</Link>
       </div>
-      <Logo />
     </div>
     <Loader type='pacman'/>
     </>
