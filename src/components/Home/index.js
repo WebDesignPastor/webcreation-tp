53e6f9ff9bc9d1acea17e93cd61911fd7e44c22f
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-05-09 16:25:16 +0200

    Changed font size on P element

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -36,27 +36,27 @@ const Home = () => {
       </div>
     </div>
     <div className="first-stage-cube-cont">
-          <div className="cubespinner">
-            <div className="face1">
-              <FontAwesomeIcon icon={faSketch} color="#DD0031" />
-            </div>
-            <div className="face2">
-              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-            </div>
-            <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-            </div>
-            <div className="face4">
-              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-            </div>
-            <div className="face5">
-              <FontAwesomeIcon icon={faJs} color="#EFD81D" />
-            </div>
-            <div className="face6">
-              <FontAwesomeIcon icon={faGithub} color="#000000" />
-            </div>
-          </div>
+      <div className="cubespinner">
+        <div className="face1">
+          <FontAwesomeIcon icon={faSketch} color="#DD0031" />
         </div>
+        <div className="face2">
+          <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+        </div>
+        <div className="face3">
+          <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+        </div>
+        <div className="face4">
+          <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+        </div>
+        <div className="face5">
+          <FontAwesomeIcon icon={faJs} color="#EFD81D" />
+        </div>
+        <div className="face6">
+          <FontAwesomeIcon icon={faGithub} color="#000000" />
+        </div>
+      </div>
+    </div>
     <Loader type='pacman'/>
     </>
   )
