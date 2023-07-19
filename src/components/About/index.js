747a68155e245913c278e6bf5d2d633817714047
Francois PARMENTIER <webcreationtp@gmail.com>
2023-07-19 15:00:27 +0200

    Updated text of about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -27,19 +27,22 @@ const About = () => {
               idx={15}
             />
           </h1>
-          <p>Profil :
-            Je suis un développeur full stack récemment diplômé du Wagon. Passionné par la technologie et la programmation,
-            j'ai acquis une solide expérience en développement web grâce à mes études et à mes projets personnels.
-            Je suis toujours à la recherche de nouveaux défis et de nouvelles opportunités pour améliorer mes compétences et développer des solutions innovantes.
+          <p>
+            François, passionné par le développement web depuis ma formation intensive au Wagon. J'ai lancé mon entreprise d'assemblage de sites
+            vitrine et d'applications web pour les auto-entrepreneurs et les petites entreprises.
           </p>
-          <p>Mes compétences :
-            Je suis compétent dans plusieurs langages de programmation, notamment Ruby, Python, JavaScript et HTML/CSS.
-            J'ai également une solide expérience en développement web front-end et back-end, ainsi qu'en intégration de bases de données.
-            J'aime travailler en équipe et j'ai une excellente capacité à résoudre les problèmes rapidement.
+          <p>
+            Je suis un développeur web full-stack, ce qui veut dire que je m'occupe autant de la partie visible de votre site web (le Front-End) que de la partie invisible (Back-end).
+            Je peux donc vous aider à créer un site web de A à Z, de la conception à la mise en ligne.
           </p>
-          <p>Mes projets :
-            Au cours de mes études au Wagon, j'ai travaillé sur plusieurs projets intéressants,
-            tels que la création d'une application web de chasse aux trésors et d'une plateforme de réservation de véhicules fictionnels en ligne.
+          <p>
+            Selon votre profil et vos attentes, je vous proposerai différentes solutions pour créer votre site web.
+            Je peux vous créer un site web sur-mesure, ou bien utiliser un CMS (Content Management System) comme Wordpress pour aller plus vite et que cela vous coûte moins cher.
+            Rendez-vous sur la page <a href="/services">Services et tarification</a> pour en savoir plus.
+
+            Je vous invite à regarder les différents <a href="/Portfolio">Projets</a> que j'ai réalisés, et à me contacter si vous avez des questions. Vous y verrez des projets complexes
+            faits pour des entreprises de taille moyenne, mais aussi des projets plus simples pour des auto-entrepreneurs. Je reste à votre disposition pour vous aider à créer votre site web.
+            N'hésitez pas à me <a href="/contact">contacter</a> pour me parler de votre projet.
           </p>
         </div>
         <div className="first-stage-cube-cont">
