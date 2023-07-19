8ac0b693624ce59f18749c12d4204fab4f9a9ab9
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-07-19 11:31:48 +0200

    Made the home page a little more responsive

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,7 +10,7 @@ const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
   const jobArray = 'Développeur Web Full Stack'.split('')
-  const greetingArray = 'Bonjour,   je suis François'.split('')
+  const greetingArray = 'Bonjour,  je suis François'.split('')
 
   useEffect(() => {
       setTimeout(() => {
@@ -23,13 +23,23 @@ const Home = () => {
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-        <AnimatedLetters letterClass={letterClass}
-        strArray={greetingArray}
-        idx={5}/>
-        <br />
-        <AnimatedLetters letterClass={letterClass}
-        strArray={jobArray}
-        idx={5}/>
+          {greetingArray.map((word, i) => (
+            <AnimatedLetters
+              key={`greeting-${i}`}
+              letterClass={letterClass}
+              strArray={word.split('')}
+              idx={5}
+            />
+          ))}
+          <br />
+          {jobArray.map((word, i) => (
+            <AnimatedLetters
+              key={`job-${i}`}
+              letterClass={letterClass}
+              strArray={word.split('')}
+              idx={5}
+            />
+          ))}
         </h1>
         <h2>Développeur Junior Full Stack / Ruby on Rails / HTML / CSS / Javascript / ReactJS </h2>
         <Link to="/contact" className='flat-button'>CONTACTEZ-MOI</Link>
